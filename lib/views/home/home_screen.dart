1a79ba028796e462bd4996d9502d39378592d3fe
sillonjeu <pius0316@naver.com>
2024-03-14 00:57:37 +0900

    feat: pdf에 서명 추가기능 구현완료

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:nightary/viewModels/home/home_viewmodel.dart';
 import 'package:nightary/views/base/base_screen.dart';
+import 'package:nightary/views/home/pdf_screen.dart';
 
 class HomeScreen extends BaseScreen<HomeViewModel> {
   const HomeScreen({super.key});
 
   @override
   Widget buildBody(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PDFViewPage()),
+          );
+        },
+        child: const Text("Home"),
+      ),
     );
   }
 }
