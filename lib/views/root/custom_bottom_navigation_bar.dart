7a195e32df77bb1b836cf176cc0320c0e6300d5e
bunju20 <ghkd2271@naver.com>
2024-03-14 23:52:14 +0900

    feat: 바텀네비게이션 선 추가, 적용은 안함

--- lib/views/root/custom_bottom_navigation_bar.dart
+++ lib/views/root/custom_bottom_navigation_bar.dart
@@ -10,47 +10,48 @@ class CustomBottomNavigationBar extends BaseWidget<RootViewModel> {
   @override
   Widget buildView(BuildContext context) {
     return Obx(
-          () => Theme(
-        data: ThemeData(
-          highlightColor: Colors.transparent,
-          splashFactory: NoSplash.splashFactory,
-        ),
-        child: BottomAppBar(
-          elevation: 0,
-          shape: const CircularNotchedRectangle(),
-          notchMargin: 6.0,
-          clipBehavior: Clip.antiAlias,
-          child: Container(
-            height: 65,
-            color: const Color(0xFFFFFFFF),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildBottomNavigationBarItem(
-                  index: 0,
-                  size: 45,
-                  svgPath: 'assets/icons/meet_g.svg',
+          () =>
+          Theme(
+            data: ThemeData(
+              highlightColor: Colors.transparent,
+              splashFactory: NoSplash.splashFactory,
+            ),
+            child: BottomAppBar(
+              elevation: 0,
+              shape: const CircularNotchedRectangle(),
+              notchMargin: 6.0,
+              clipBehavior: Clip.antiAlias,
+              child: Container(
+                height: 65,
+                color: const Color(0xFFFFFFFF),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildBottomNavigationBarItem(
+                      index: 0,
+                      size: 45,
+                      svgPath: 'assets/icons/meet_g.svg',
+                    ),
+                    _buildBottomNavigationBarItem(
+                      index: 1,
+                      size: 45,
+                      svgPath: 'assets/icons/chat.svg',
+                    ),
+                    _buildBottomNavigationBarItem(
+                      index: 2,
+                      size: 45,
+                      svgPath: 'assets/icons/like.svg',
+                    ),
+                    _buildBottomNavigationBarItem(
+                      index: 3,
+                      size: 45,
+                      svgPath: 'assets/icons/my.svg',
+                    ),
+                  ],
                 ),
-                _buildBottomNavigationBarItem(
-                  index: 1,
-                  size: 45,
-                  svgPath: 'assets/icons/chat.svg',
-                ),
-                _buildBottomNavigationBarItem(
-                  index: 2,
-                  size: 45,
-                  svgPath: 'assets/icons/like.svg',
-                ),
-                _buildBottomNavigationBarItem(
-                  index: 3,
-                  size: 45,
-                  svgPath: 'assets/icons/my.svg',
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
     );
   }
 
@@ -60,12 +61,36 @@ class CustomBottomNavigationBar extends BaseWidget<RootViewModel> {
     required String svgPath,
   }) =>
       Expanded(
-        child: SvgPicture.asset(
-          svgPath,
-          height: size,
-          colorFilter: viewModel.selectedIndex == index
-              ? const ColorFilter.mode(Color(0xFF634EC0), BlendMode.srcATop)
-              : const ColorFilter.mode(Colors.transparent, BlendMode.srcATop),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(
+              svgPath,
+              height: size,
+              colorFilter: viewModel.selectedIndex == index
+                  ? const ColorFilter.mode(Color(0xFF634EC0), BlendMode.srcATop)
+                  : const ColorFilter.mode(
+                  Colors.transparent, BlendMode.srcATop),
+            ),
+          ],
         ),
       );
+
+  Widget _buildBottomNavigationBarLine({
+    required int index,
+    required double size,
+  }) =>
+      Container(
+        child: viewModel.selectedIndex == index
+            ? Container(// 선과 아이콘 사이의 간격 조절
+          height: 2.0, // 선의 높이
+          width: size, // 선의 너비를 아이콘의 크기에 맞추거나 조절
+          color: Color(0xFFA4A3E1), // 선의 색상
+        )
+            : Container(),
+      );
+
+
 }
+
+
