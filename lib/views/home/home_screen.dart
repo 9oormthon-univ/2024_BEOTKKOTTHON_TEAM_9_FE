4781883c05c83a86f83a7233c64deb65affd074e
sillonjeu <pius0316@naver.com>
2024-03-15 02:25:57 +0900

    feat: 개인 정보 동의(뷰 생성, 동의 체크 박스, 서명 구현) 완료

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nightary/viewModels/home/home_viewmodel.dart';
 import 'package:nightary/views/base/base_screen.dart';
-import 'package:nightary/views/home/pdf_screen.dart';
+import '../widget/privacy_consent_screen.dart';
 
 class HomeScreen extends BaseScreen<HomeViewModel> {
   const HomeScreen({super.key});
@@ -13,7 +13,7 @@ class HomeScreen extends BaseScreen<HomeViewModel> {
         onTap: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => PDFViewPage()),
+            MaterialPageRoute(builder: (context) => OnboardingScreen()),
           );
         },
         child: const Text("Home"),
