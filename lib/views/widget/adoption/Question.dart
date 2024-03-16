a214ccc27ad140ffb953125d16a6f18a62de70d5
sillonjeu <pius0316@naver.com>
2024-03-16 19:17:08 +0900

    입양 신청서 스크린 생성 완료

--- lib/views/widget/adoption/Question.dart
+++ lib/views/widget/adoption/Question.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controller/responses_controller.dart';
+import '../privacy/privacy_consent_screen.dart';
 
 // 상태 관리를 위한 ResponsesController 인스턴스 생성
 final ResponsesController responsesController = Get.put(ResponsesController());
@@ -397,6 +398,10 @@ class Q4 extends StatelessWidget {
               onPressed: () {
                 // 사용자가 입력한 텍스트를 responsesController에 저장
                 responsesController.addResponse({"question4": _controller.text});
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => OnboardingScreen(2)),
+                );
               },
               child: Text('제출 할게요!',
                 style: TextStyle(
